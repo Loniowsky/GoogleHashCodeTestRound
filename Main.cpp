7e28b510177ec469411926b19813be2c2529ddbb
Loniowsky <jjcierpich@interia.pl>
2018-02-21 15:58:57 +0100

    Work frame and dependency clear

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
+#include "Main.h"
+
+int rows = 2;
+int columns = 2;
 
 int main(){
+	std::vector<std::vector<bool>> v;
+	v.emplace_back();
+	v.emplace_back();
+	v[0].emplace_back(0);
+	v[0].emplace_back(1);
+	v[1].emplace_back(1);
+	v[1].emplace_back(0);
 
 
-
+	PizzaSeparator sep(v);
 }
--- Point.h
+++ Point.h
@@ -3,6 +3,7 @@
 class Point{
 public:
 	Point(unsigned int x = 0, unsigned int y = 0):m_x(x),m_y(y){};
+	
 private:
 	int m_x;
 	int m_y;
